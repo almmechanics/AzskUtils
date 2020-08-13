06946d292960838d2944a4f3ae3df4a3e55e48af
Martin Palmer <martin.palmer@almmechanics.com>
2020-08-14 00:20:33 +0100

    Moved to vssdk library, and upped agent requirements

--- tasks/Azsk-NUnit/action.ps1
+++ tasks/Azsk-NUnit/action.ps1
@@ -1,34 +1,38 @@
-param
-(
-    [string]
-    $AzskOutputFolder,
-    [string]
-    $OutputVariable,
-    [string]
-    $EnableExit
-)
+[CmdletBinding()]
+param()
 
-$moduleFolder = "$pwd\modules"
+Trace-VstsEnteringInvocation $MyInvocation
+try 
+{   
+    Import-VstsLocStrings "$PSScriptRoot\Task.json"
 
-Write-Host ('Loading azskhelper module from {0}' -f $moduleFolder)
-Import-Module $moduleFolder\azskhelper\azskhelper.psd1 -verbose
+    # Get task variables.
+    [string]$AzskOutputFolder = Get-VstsInput -Name AzskOutputFolder
+    [bool]$EnableExit = Get-VstsInput -Name EnableExit -AsBool
 
-if (get-module -name 'azskhelper')
+    Import-Module -name $PSScriptRoot\ps_modules\azskhelper\azskhelper.psd1 -verbose
+
+    if (get-module -name 'azskhelper')
+    {
+      Write-Host 'azskhelper module loaded'
+
+      if ($EnableExit)
+      {
+          Write-Verbose 'Publish-AzskNUnit with Exit Enabled'
+          Publish-AzskNUnit -Path $AzskOutputFolder -EnableExit
+      }
+      else
+      {
+          Write-Verbose 'Publish-AzskNUnit without Exit Enabled'
+          Publish-AzskNUnit -Path $AzskOutputFolder
+      }
+    }
+    else
+    {
+      Write-Host "##vso[task.logissue type=error;]Unable to load azskhelper module"
+    }
+  } 
+finally 
 {
-  Write-Host 'azskhelper module loaded'
-
-  if ($EnableExit -eq [bool]::TrueString)
-  {
-      Write-Verbose 'Publish-AzskNUnit with Exit Enabled'
-      Publish-AzskNUnit -Path $AzskOutputFolder -EnableExit
-  }
-  else
-  {
-      Write-Verbose 'Publish-AzskNUnit without Exit Enabled'
-      Publish-AzskNUnit -Path $AzskOutputFolder
-  }
-}
-else
-{
-  Write-Host "##vso[task.logissue type=error;]Unable to load azskhelper module"
+  Trace-VstsLeavingInvocation $MyInvocation
 }
--- tasks/Azsk-svt-NUnit/action.ps1
+++ tasks/Azsk-svt-NUnit/action.ps1
@@ -1,34 +1,38 @@
-param
-(
-    [string]
-    $AzskOutputFolder,
-    [string]
-    $OutputVariable,
-    [string]
-    $EnableExit
-)
+[CmdletBinding()]
+param()
 
-$moduleFolder = "$pwd\modules"
+Trace-VstsEnteringInvocation $MyInvocation
+try 
+{   
+  Import-VstsLocStrings "$PSScriptRoot\Task.json"
 
-Write-Host ('Loading azskhelper module from {0}' -f $moduleFolder)
-Import-Module $moduleFolder\azskhelper\azskhelper.psd1 -verbose
+  # Get task variables.
+  [string]$AzskOutputFolder = Get-VstsInput -Name AzskOutputFolder
+  [bool]$EnableExit = Get-VstsInput -Name EnableExit -AsBool
 
-if (get-module -name 'azskhelper')
-{
-  Write-Host 'azskhelper module loaded'
+  Import-Module -name $PSScriptRoot\ps_modules\azskhelper\azskhelper.psd1 -verbose
 
-  if ($EnableExit -eq [bool]::TrueString)
+  if (get-module -name 'azskhelper')
   {
-      Write-Verbose 'Publish-AzskSVTNUnit with Exit Enabled'
-      Publish-AzskSVTNUnit -Path $AzskOutputFolder -EnableExit
+    Write-Host 'azskhelper module loaded'
+
+    if ($EnableExit)
+    {
+        Write-Verbose 'Publish-AzskSVTNUnit with Exit Enabled'
+        Publish-AzskSVTNUnit -Path $AzskOutputFolder -EnableExit
+    }
+    else
+    {
+        Write-Verbose 'Publish-AzskSVTNUnit without Exit Enabled'
+        Publish-AzskSVTNUnit -Path $AzskOutputFolder
+    }
   }
   else
   {
-      Write-Verbose 'Publish-AzskSVTNUnit without Exit Enabled'
-      Publish-AzskSVTNUnit -Path $AzskOutputFolder
+    Write-Host "##vso[task.logissue type=error;]Unable to load azskhelper module"
   }
 }
-else
+finally 
 {
-  Write-Host "##vso[task.logissue type=error;]Unable to load azskhelper module"
+  Trace-VstsLeavingInvocation $MyInvocation
 }
